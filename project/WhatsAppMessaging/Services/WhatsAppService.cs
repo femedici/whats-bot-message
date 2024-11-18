5e6328fc354e392400e7ed2b31e1951fd8bd99e6
femedici <ic1dem.contato@gmail.com>
2024-11-18 16:31:12 -0300

    fix services

--- project/WhatsAppMessaging/Services/WhatsAppService.cs
+++ project/WhatsAppMessaging/Services/WhatsAppService.cs
@@ -10,37 +10,40 @@ namespace WhatsAppMessaging.Services
         public async Task<string> SendMessageAsync(Message message)
         {
             var url = $"{Config.BaseUrl}{Config.InstanceId}/messages/chat";
-            var client = new RestClient(url);
-            var request = new RestRequest(Method.Post);
-            
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddParameter("token", Config.Token);
-            request.AddParameter("to", message.To);
-            request.AddParameter("body", message.Body);
+            var client = new RestClient();
+            var request = new RestRequest(url, Method.Post);
 
+            // Configurar os headers e parâmetros
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("token", Config.Token, ParameterType.GetOrPost);
+            request.AddParameter("to", message.To, ParameterType.GetOrPost);
+            request.AddParameter("body", message.Body, ParameterType.GetOrPost);
+
+            // Executar a requisição
             var response = await client.ExecuteAsync(request);
-            return response.Content;
+            return response.Content ?? string.Empty;
         }
 
         public async Task<string> SendMediaMessageAsync(Message message, string mediaType)
         {
             var url = $"{Config.BaseUrl}{Config.InstanceId}/messages/{mediaType}";
-            var client = new RestClient(url);
-            var request = new RestRequest(Method.Post);
+            var client = new RestClient();
+            var request = new RestRequest(url, Method.Post);
 
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddParameter("token", Config.Token);
-            request.AddParameter("to", message.To);
-            request.AddParameter("body", message.Body);
+            // Configurar os headers e parâmetros
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("token", Config.Token, ParameterType.GetOrPost);
+            request.AddParameter("to", message.To, ParameterType.GetOrPost);
 
             if (!string.IsNullOrEmpty(message.MediaUrl))
-                request.AddParameter(mediaType, message.MediaUrl);
+                request.AddParameter(mediaType, message.MediaUrl, ParameterType.GetOrPost);
 
             if (!string.IsNullOrEmpty(message.Caption))
-                request.AddParameter("caption", message.Caption);
+                request.AddParameter("caption", message.Caption, ParameterType.GetOrPost);
 
+            // Executar a requisição
             var response = await client.ExecuteAsync(request);
-            return response.Content;
+            return response.Content ?? string.Empty;
         }
     }
 }
